6b223fd5083bef916f2c8454c231fe53dbe1aa5b
Canaris <mrgeek484@gmail.com>
2023-05-10 09:56:04 +0800

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,21 @@
+import './globals.css';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata = {
+	title: 'Todos',
+	description: 'A simple todo app that stores todos in Redis',
+};
+
+export default function RootLayout({
+	children,
+}: {
+  children: React.ReactNode
+}) {
+	return (
+		<html lang='en'>
+			<body className={inter.className}>{children}</body>
+		</html>
+	);
+}
---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+	return (
+		<main className='flex min-h-screen flex-col p-24'>
+			<div className='z-10 w-full font-mono text-sm lg:flex'>
+				<h1 className='w-full text-4xl font-bold text-center text-gray-300'>
+					Todos App
+				</h1>
+			</div>
+		</main>
+	);
+}
